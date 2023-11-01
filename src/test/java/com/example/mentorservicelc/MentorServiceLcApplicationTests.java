b4bfbe57dcb65652ef8f8a3ac1db779437c5c0c6
theali <thealiergashev@gmail.com>
2023-11-01 21:28:27 +0500

    Initial commit

---
+++ src/test/java/com/example/mentorservicelc/MentorServiceLcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentorservicelc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorServiceLcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
